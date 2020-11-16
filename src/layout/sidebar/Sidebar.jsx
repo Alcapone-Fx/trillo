331e294127335d6b0d809c1d5e7d1218e8fb5d50
Felix Navas <navasfx19@gmail.com>
2020-11-16 10:42:37 -0600

    :sparkles: Add sidebar navigation structure

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-import "./Sidebar.scss";
+import sprite from '../../assets/img/sprite.svg';
+import './Sidebar.scss';
 
 function Sidebar() {
-    return (
-        <nav className="sidebar">
-            Navigation
-        </nav>
-    )
+  return (
+    <nav className="sidebar">
+      <ul className="side-nav">
+        <li className="side-nav__item">
+          <a href="/" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-home`}></use>
+            </svg>
+            <span>Hotel</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="/" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-aircraft-take-off`}></use>
+            </svg>
+            <span>Flight</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="/" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-key`}></use>
+            </svg>
+            <span>Car rental</span>
+          </a>
+        </li>
+        <li className="side-nav__item">
+          <a href="/" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-map`}></use>
+            </svg>
+            <span>Tours</span>
+          </a>
+        </li>
+      </ul>
+      <div className="legal">&copy; 2020 by Trillo. All rights reserved.</div>
+    </nav>
+  );
 }
 
 export default Sidebar;
