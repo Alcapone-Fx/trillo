069a4a760fee5a16773aa52f080163494d6b8b14
Felix Navas <fnavas@applaudostudios.com>
2021-01-14 17:49:15 -0600

    :recycle: Refactor code

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -4,41 +4,33 @@ import sprite from '../../assets/img/sprite.svg';
 import './Sidebar.scss';
 
 function Sidebar() {
+  const iconTextSideNavArray = [
+    { icon: 'home', text: 'Hotel' },
+    { icon: 'aircraft-take-off', text: 'Flight' },
+    { icon: 'key', text: 'Car rental' },
+    { icon: 'map', text: 'Tours' },
+  ];
+
   return (
     <nav className="sidebar">
       <ul className="side-nav">
-        <li className="side-nav__item side-nav__item--active">
-          <a href="/" className="side-nav__link">
-            <svg className="side-nav__icon">
-              <use xlinkHref={`${sprite}#icon-home`}></use>
-            </svg>
-            <span>Hotel</span>
-          </a>
-        </li>
-        <li className="side-nav__item">
-          <a href="/" className="side-nav__link">
-            <svg className="side-nav__icon">
-              <use xlinkHref={`${sprite}#icon-aircraft-take-off`}></use>
-            </svg>
-            <span>Flight</span>
-          </a>
-        </li>
-        <li className="side-nav__item">
-          <a href="/" className="side-nav__link">
-            <svg className="side-nav__icon">
-              <use xlinkHref={`${sprite}#icon-key`}></use>
-            </svg>
-            <span>Car rental</span>
-          </a>
-        </li>
-        <li className="side-nav__item">
-          <a href="/" className="side-nav__link">
-            <svg className="side-nav__icon">
-              <use xlinkHref={`${sprite}#icon-map`}></use>
-            </svg>
-            <span>Tours</span>
-          </a>
-        </li>
+        {iconTextSideNavArray.map(({ icon, text }) => {
+          let liClassName =
+            icon === 'home'
+              ? 'side-nav__item side-nav__item--active'
+              : 'side-nav__item';
+
+          return (
+            <li className={liClassName} key={icon}>
+              <a href="/" className="side-nav__link">
+                <svg className="side-nav__icon">
+                  <use xlinkHref={`${sprite}#icon-${icon}`}></use>
+                </svg>
+                <span>{text}</span>
+              </a>
+            </li>
+          );
+        })}
       </ul>
       <div className="legal">&copy; 2020 by Trillo. All rights reserved.</div>
     </nav>
