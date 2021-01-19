f7d554fc9cdc86e64908fd77ae6b9b03891212d6
Felix Navas <fnavas@applaudostudios.com>
2021-01-19 15:27:28 -0600

    Updates

---
+++ precache-manifest.a5cd303067129bb1ca34185ee4118b12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee0479867b4a17e8cc350c82f3bb750",
+    "url": "/trillo/index.html"
+  },
+  {
+    "revision": "073a1f8d407e87a51aa8",
+    "url": "/trillo/static/css/main.5a8b4f2e.chunk.css"
+  },
+  {
+    "revision": "d033b0ac2aacda9d9914",
+    "url": "/trillo/static/js/2.35947a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trillo/static/js/2.35947a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073a1f8d407e87a51aa8",
+    "url": "/trillo/static/js/main.86a1e99b.chunk.js"
+  },
+  {
+    "revision": "ce28dc5b04739f9e6aa7",
+    "url": "/trillo/static/js/runtime-main.6fded000.js"
+  },
+  {
+    "revision": "6f98acff3163c6603e8a52b1e97e3f7a",
+    "url": "/trillo/static/media/chevron-thin-right.6f98acff.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillo/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillo/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillo/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "79514b076fb1acd082b93539344d3650",
+    "url": "/trillo/static/media/sprite.79514b07.svg"
+  },
+  {
+    "revision": "a2a26d7906c257d584688e4a0130fe8c",
+    "url": "/trillo/static/media/user.a2a26d79.jpg"
+  }
+]);
